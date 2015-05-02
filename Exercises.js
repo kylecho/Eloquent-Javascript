fd9a04a6d699296129994465ba4a4e9105b06d21
Kyle Cho <kylecho.work@gmail.com>
2015-05-01 23:31:50 -0700

    add solution

--- Exercises.js
+++ Exercises.js
@@ -390,3 +390,56 @@ function arrayToList(array) {
 // nth
 */
 
+// A list
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest)
+    array.push(node.value);
+  return array;
+}
+
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+
+// Deep comparison
+var obj = {here: {is: 'an'}, object: 2};
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (a == null || typeof a != 'object' ||
+      b == null || typeof b != 'object')
+    return false;
+
+  var propsInA = 0, propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (var prop in b) {
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+
+  return propsInA == propsInB;
+}
+
